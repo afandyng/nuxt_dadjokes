6e40faee604903a26825cfea2bdfb2a961f54670
Afandy Ng <ng.afandy@gmail.com>
2021-01-07 10:47:10 -0500

    Initial commit

---
+++ pages/about.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h1>About DadJokes</h1>
+    <p>
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nemo doloribus
+      repudiandae voluptatibus deserunt blanditiis! Tenetur minus doloribus sit
+      inventore? Omnis!
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  head() {
+    return {
+      title: "About The App",
+      meta: [
+        {
+          hid: "description",
+          name: "description",
+          content: "Best place for corny dad jokes"
+        }
+      ]
+    };
+  }
+};
+</script>
+
+<style></style>
